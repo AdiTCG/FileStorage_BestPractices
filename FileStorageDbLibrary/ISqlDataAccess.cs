a9c9bc8f8bb2645f014ef85a66e2d0f5371dca94
AdiTCG <aditheOrigin@gmail.com>
2023-02-13 11:00:53 +0530

    Extracted interface for better unit tested and mocking interface.

---
+++ FileStorageDbLibrary/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace FileStorageDbLibrary
+{
+    internal interface ISqlDataAccess
+    {
+        Task SaveData(string storeProcedure, string connectionName, object parameters);
+    }
+}
--- FileStorageDbLibrary/SqlDataAccess.cs
+++ FileStorageDbLibrary/SqlDataAccess.cs
@@ -4,29 +4,29 @@ using Microsoft.Extensions.Configuration;
 
 namespace FileStorageDbLibrary;
 
-internal class SqlDataAccess
+internal class SqlDataAccess : ISqlDataAccess
 {
     private IConfiguration _configuration;
 
-	public SqlDataAccess(IConfiguration configuration)
-	{
-		_configuration= configuration;
-	}
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
 
-	public async Task SaveData(
-		string storeProcedure,
-		string connectionName,
-		object parameters)
-	{
-		string connectionString = _configuration.GetConnectionString(connectionName)
-			?? throw new Exception($"Missing connection string at {connectionName}");
+    public async Task SaveData(
+        string storeProcedure,
+        string connectionName,
+        object parameters)
+    {
+        string connectionString = _configuration.GetConnectionString(connectionName)
+            ?? throw new Exception($"Missing connection string at {connectionName}");
 
-		using var connection = new SqlConnection(connectionString);
-		
-		await connection.ExecuteAsync(
-			storeProcedure, 
-			parameters, 
-			commandType: System.Data.CommandType.StoredProcedure);
+        using var connection = new SqlConnection(connectionString);
 
-	}
+        await connection.ExecuteAsync(
+            storeProcedure,
+            parameters,
+            commandType: System.Data.CommandType.StoredProcedure);
+
+    }
 }
